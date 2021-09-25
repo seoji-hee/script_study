d4702c484e9e65b292003c8e361a6703f76bee7e
seoji-hee <71322224+seoji-hee@users.noreply.github.com>
2021-09-25 17:15:45 +0900

    ver3

---
+++ 03-morden-javascript/01_javascript.js
@@ -0,0 +1,18 @@
+let num_1 = 200;
+let num_2 = 100;
+
+//두 변수의 나머지연산 결과를 alert()으로 출력
+let mod = num_1 % num_2;//0
+alert(mod);
+
+
+//대입 또는 할당연산자
+let buscard = 1;
+
+buscard = buscard + 1;//2 누적(버카충)
+
+buscard += 1;//3
+
+buscard++;//4 증감연산자
+
+console.log(`buscard = ${buscard}`);
